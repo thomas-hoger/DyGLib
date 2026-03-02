2e6ececf1b497e041a852d80fb27a7ce932bd299
thoger <thomas.hoger@laas.fr>
2026-03-02 17:39:47 +0100

    data

--- utils/load_configs.py
+++ utils/load_configs.py
@@ -16,7 +16,7 @@ def get_link_prediction_args(is_evaluation: bool = False):
     parser.add_argument('--batch_size', type=int, default=200, help='batch size')
     parser.add_argument('--model_name', type=str, default='DyGFormer', help='name of the model, note that EdgeBank is only applicable for evaluation',
                         choices=['JODIE', 'DyRep', 'TGAT', 'TGN', 'CAWN', 'EdgeBank', 'TCL', 'GraphMixer', 'DyGFormer'])
-    parser.add_argument('--gpu', type=int, default=0, help='number of gpu to use')
+    parser.add_argument('--gpu', type=int, default=1, help='number of gpu to use')
     parser.add_argument('--num_neighbors', type=int, default=20, help='number of neighbors to sample for each node')
     parser.add_argument('--sample_neighbor_strategy', type=str, default='recent', choices=['uniform', 'recent', 'time_interval_aware'], help='how to sample historical neighbors')
     parser.add_argument('--time_scaling_factor', default=1e-6, type=float, help='the hyperparameter that controls the sampling preference with time interval, '
