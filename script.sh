346cc413ca898fbc10df44004b1ff7ff9cfff313
thoger <thomas.hoger@laas.fr>
2026-05-20 11:06:22 +0200

    matrix

--- script.sh
+++ script.sh
@@ -2,10 +2,12 @@
 
 # models=('DyGFormer' 'GraphMixer' 'TGAT' 'TCL' 'CAWN' 'TGN' 'JODIE' 'DyRep') 
 
-models=('GraphMixer' 'TGAT' 'TCL' 'CAWN' 'TGN' 'JODIE' 'DyRep') 
+models=('TCL' 'CAWN'  'DyGFormer' 'JODIE')  #  'DyRep' 'GraphMixer'
+# models=('GraphMixer' 'TGAT' 'TCL' 'CAWN' 'TGN' 'JODIE' 'DyRep') 
+
 
 for model in "${models[@]}"; do
     # python train_reconstruction_CTD5G.py --num_epochs=2 --model_name="$model"
     # python train_link_prediction_CTD5G.py --num_epochs=5 --model_name="$model" --time_feat_dim=30 --expe_name=baseline
-    python eval_link_prediction_CTD5G.py --model_name="$model" --time_feat_dim=30 --expe_name=baseline
+    python eval_link_prediction_CTD5G.py --model_name="$model" --dataset_name=old_CTD5G --time_feat_dim=30 --expe_name=supervised
 done
--- script_tgn.sh
+++ script_tgn.sh
@@ -2,10 +2,11 @@
 
 
 # expe=('small_no_time' 'small_no_time_pid' 'small_no_time_pid_sampling') 
-expe=('0_001_lr' '40_neighbor' 'accuracy' 'batch_500' 'mse') 
+# 'accuracy' 'mse'
+expe=('supervised_0_001_lr' 'supervised_40_neighbor'  'supervised_batch_500') 
 
 for expe_name in "${expe[@]}"; do
-    python eval_link_prediction_CTD5G.py --model_name=TGN --expe_name="$expe_name" --time_feat_dim=30
+    python eval_link_prediction_CTD5G.py --model_name=TGN --expe_name="$expe_name" --time_feat_dim=30 --dataset_name=old_CTD5G
 done
 
 # python eval_link_prediction_CTD5G.py --model_name=TGN --expe_name=supervised --time_feat_dim=30 --dataset_name=old_CTD5G
