aecb0498714521a209b5c8efed3a4e6af4d87615
thoger <thomas.hoger@laas.fr>
2026-06-30 18:00:07 +0200

    script

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ for model in "${models[@]}"; do
 
     /usr/bin/time -f "Temps réel: %E\nTemps CPU: %U user %S sys\nMémoire max: %M KB" \
         -o "$logfile" -a \
-        python eval_model_CTD5G.py --model_name="$model"
+        python eval_reconstruction_CTD5G.py  --model_name="$model" --expe_name=supervised --dataset_name=old_CTD5G --time_feat_dim=30
 
     echo "" >> "$logfile"
 done
