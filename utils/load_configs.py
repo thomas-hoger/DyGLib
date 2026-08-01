9dd288b8264a0aa5407fbf874c6b02c51d848b26
thoger <thomas.hoger@laas.fr>
2026-08-01 17:05:41 +0200

    load config

--- utils/load_configs.py
+++ utils/load_configs.py
@@ -12,7 +12,7 @@ def get_link_prediction_args(is_evaluation: bool = False):
     # arguments
     parser = argparse.ArgumentParser('Interface for the link prediction task')
     parser.add_argument('--expe_name', type=str, default='default', help='name of the experiment')
-    parser.add_argument('--supervised', type=bool, help='is it supervised learning')
+    parser.add_argument('--supervised', type=bool, action=argparse.BooleanOptionalAction, help='is it supervised learning')
     parser.add_argument('--dataset_name', type=str, help='dataset to be used', default='CTD5G')
     parser.add_argument('--batch_size', type=int, default=200, help='batch size')
     parser.add_argument('--model_name', type=str, default='DyGFormer', help='name of the model, note that EdgeBank is only applicable for evaluation',
